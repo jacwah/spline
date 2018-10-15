989a96fd18a92fe7c4dbd7a4fcc97c5b40268a98
Jacob Wahlgren <jacob@dstsrc.net>
2018-10-15 21:10:24 +0200

    Draw circle at mouse pos

--- spline.cpp
+++ spline.cpp
@@ -1,6 +1,22 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 
+void
+draw_circle(uint8_t *pix, int x, int y, int rsq, int color)
+{
+    for (int py = 0; py < 480; py++)
+        for (int px = 0; px < 640; px++) {
+            int dx = px - x;
+            int dy = py - y;
+            if (dx*dx + dy*dy <= rsq) {
+                uint8_t *bufpos = pix + 3*(640*py + px);
+                *bufpos++ = color & 0x0000ff;
+                *bufpos++ = color & 0x00ff00;
+                *bufpos++ = color & 0xff0000;
+            }
+        }
+}
+
 int
 main(int argc, char **argv)
 {
@@ -13,7 +29,7 @@ main(int argc, char **argv)
             480,
             0);
 
-    SDL_Surface *canvas = SDL_CreateRGBSurface(
+    SDL_Surface *canvas_surf = SDL_CreateRGBSurface(
             0,
             640,
             480,
@@ -22,39 +38,39 @@ main(int argc, char **argv)
             0x00ff00,
             0xff0000,
             0);
-    uint8_t *pix = (uint8_t *) canvas->pixels;
-    for (int y = 0; y < 480; y++)
-        for (int x = 0; x < 640; x++) {
-            int r = 0;
-            int g = 0;
-            int b = 0;
-            switch ((x/8 + y/8) % 3) {
-                case 0:
-                    r = 0xff;
-                    break;
-                case 1:
-                    g = 0xff;
-                    break;
-                case 2:
-                    b = 0xff;
-                    break;
-            }
-            *pix++ = r;
-            *pix++ = g;
-            *pix++ = b;
-        }
+    uint8_t *canvas = (uint8_t *) canvas_surf->pixels;
+    memset(canvas, 0xff, 640*480*3);
+
     SDL_Surface *windowsurf = SDL_GetWindowSurface(window);
-    SDL_BlitSurface(canvas, NULL, windowsurf, NULL);
+    SDL_BlitSurface(canvas_surf, NULL, windowsurf, NULL);
 
     bool quit = false;
+    int mousex, mousey;
     while (!quit) {
+        bool have_mouse_pos = false;
         SDL_Event ev;
         while (SDL_PollEvent(&ev)) {
             switch (ev.type) {
-                case SDL_QUIT:
-                    quit = true;
-                    break;
+            case SDL_QUIT:
+                quit = true;
+                break;
+            case SDL_MOUSEMOTION:
+                {
+                    int x = ev.motion.x;
+                    int y = ev.motion.y;
+                    if (x > 0 && x < 640 && y > 0 && y < 480) {
+                        have_mouse_pos = true;
+                        mousex = x;
+                        mousey = y;
+                    }
+                }
+                break;
             }
+        }
+        if (have_mouse_pos) {
+            memset(canvas, 0xff, 640*480*3);
+            draw_circle(canvas, mousex, mousey, 32, 0);
+            SDL_BlitSurface(canvas_surf, NULL, windowsurf, NULL);
         }
         SDL_UpdateWindowSurface(window);
     }
